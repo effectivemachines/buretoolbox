02fb8a049eeccd4b56bccba98a9c345451bf0ec7
Allen Wittenauer <aw@apache.org>
2018-12-29 09:36:05 -0800

    2

--- precommit/src/main/shell/smart-apply-patch.sh
+++ precommit/src/main/shell/smart-apply-patch.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Make sure that bash version meets the pre-requisite
 
@@ -312,7 +325,7 @@ if [[ ${COMMITMODE} = true ]]; then
   PATCH_METHODS=("gitam" "${PATCH_METHODS[@]}")
 fi
 
-patchfile_dryrun_driver "${INPUT_PATCH_FILE}"
+patchfile_dryrun_driver "${PATCH_DIR}/patch"
 RESULT=$?
 
 if [[ ${RESULT} -gt 0 ]]; then
@@ -321,7 +334,7 @@ if [[ ${RESULT} -gt 0 ]]; then
 fi
 
 if [[ ${PATCH_DRYRUNMODE} == false ]]; then
-  patchfile_apply_driver "${INPUT_PATCH_FILE}" "${GPGSIGN}"
+  patchfile_apply_driver "${PATCH_DIR}/patch" "${GPGSIGN}"
   RESULT=$?
 fi
 
